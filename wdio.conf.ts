8fe72f0274def5558352307d2b117782cad750ef
marina51533 <marina51533@gmail.com>
2024-09-18 08:45:34 +0200

    test1

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -31,8 +31,6 @@ export const config: WebdriverIO.Config = {
   // Test Configurations
   // ===================
   // Define all options that are relevant for the WebdriverIO instance here
-  //
-  // Level of logging verbosity: trace | debug | info | warn | error | silent
   logLevel: "info",
   //
   // Set specific log levels per logger
