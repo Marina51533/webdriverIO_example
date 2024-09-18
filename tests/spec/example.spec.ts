a54ab8378de05eb692275d4918367eb02664fb7a
marina51533 <marina51533@gmail.com>
2024-09-18 08:35:31 +0200

    test1

--- tests/spec/example.spec.ts
+++ tests/spec/example.spec.ts
@@ -12,7 +12,7 @@ describe("Performing operations on MoroSystems via Google", () => {
     // Validate Google Search Page
     expect(await browser.getTitle()).to.equal("Google");
 
-    // Perform search and validate result
+    // Perform search and validate results
     await googleSearchPage.search("MoroSystems");
     await googleSearchPage.isSearched();
     const isTitleDisplayed = await googleSearchPage.morosystemsLink.isDisplayed()
